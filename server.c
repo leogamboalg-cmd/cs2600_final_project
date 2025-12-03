9c586f5f71ea1838013d73325f9aecd94c4c81b1
nickmscott24 <nickmscott24@gmail.com>
2025-12-02 19:19:09 -0800

    Update server.c

--- server.c
+++ server.c
@@ -142,3 +142,168 @@ int main() {
 
         return 0;
 } // end main
+nmscott@login-02 ~/cs2600/project2 $ gcc chat_server.c -lpthread && ./a.out
+Listening on port 3264 for clients...
+one
+two
+hi
+leo
+gam
+hi
+yo
+^C
+nmscott@login-02 ~/cs2600/project2 $ vim chat_server.c
+nmscott@login-02 ~/cs2600/project2 $ gcc chat_server.c -lpthread && ./a.out
+Listening on port 3264 for clients...
+one
+two
+hi
+nmscott@login-02 ~/cs2600/project2 $ gcc chat_server.c -lpthread && ./a.out
+bind: Address already in use
+nmscott@login-02 ~/cs2600/project2 $ cat chat_server.c
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sock.h"
+
+int clientArr[MaxConnects];
+int clientNum = 0;
+pthread_t threads[MaxConnects];
+pthread_mutex_t clientMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t listeningMutex = PTHREAD_MUTEX_INITIALIZER;
+
+void report(const char* msg, int terminate) {
+        perror(msg);
+        if (terminate) exit(-1); // failure
+} // end report
+
+// send/receive cycle for connected clients
+void *clientFunc(void *ptr) {
+        char buffer[BuffSize + 1];
+        char tempBuffer[BuffSize + 1];
+        int client_fd = *((int*) ptr);
+        FILE *fp;
+        int firstMessage = 1;
+        char username[BuffSize + 1];
+        char *inBetween = ": ";
+
+        // constantly send to and receive from client
+        while(1) {
+                memset(buffer, '\0', sizeof(buffer)); // reset buffer
+                int count = recv(client_fd, &buffer, sizeof(buffer), 0);
+                if (count > 0) { // received a message
+                        puts(buffer);
+
+                        // save username
+                        if (firstMessage == 1) {
+                                strcpy(username, buffer);
+
+                                int curr_client;
+                                strcat(buffer, " has joined the chat!");
+
+                                for (int i = 0; i < MaxConnects; ++i) {
+                                        curr_client = clientArr[i];
+                                        if (curr_client > 0 && curr_client != client_fd)
+                                                send(curr_client, &buffer, sizeof(buffer), 0);
+                                }
+                                firstMessage = 0;
+                        } // end if
+                        else {
+                                strcpy(tempBuffer, username);
+                                strcat(tempBuffer, inBetween);
+                                strcat(tempBuffer, buffer);
+                                int curr_client;
+                                for (int i = 0; i < MaxConnects; ++i) {
+                                        curr_client = clientArr[i];
+                                        if (curr_client > 0 && curr_client != client_fd) // on valid address
+                                                send(curr_client, &tempBuffer, sizeof(tempBuffer), 0);
+                                } // end for
+
+                                if (strcmp(buffer, "exit") == 0) {
+                                        pthread_mutex_unlock(&clientMutex);
+                                        break;
+                                } // end if
+                                else {
+                                        // save to chat history
+                                        pthread_mutex_lock(&clientMutex);
+                                        fp = fopen("chat_history", "a");
+                                        fprintf(fp, "%s\n", buffer);
+                                        fclose(fp);
+                                        pthread_mutex_unlock(&clientMutex);
+                                } // end else
+                        }
+                } // end if
+        } // end while
+} // end clientFunc
+
+// listen for clients
+void *listening(void *ptr) {
+        while (1) { // listen indefinitely
+                int fd = *((int*) ptr);
+                struct sockaddr_in caddr; // client address
+                int len = sizeof(caddr); // address length could change
+
+                int client_fd = accept(fd, (struct sockaddr*) &caddr, &len);
+                if (client_fd < 0) {
+                        report("accept", 0);
+                        continue;
+                }
+                else { // valid client address
+                        pthread_mutex_lock(&listeningMutex);
+                        pthread_t clientThread;
+                        threads[clientNum] = clientThread;
+
+                        clientArr[clientNum] = client_fd;
+                        clientNum++;
+                        pthread_mutex_unlock(&listeningMutex);
+                        // spawn new thread for each connected client
+                        pthread_create(&clientThread, NULL, clientFunc, (void *) &client_fd);
+                } // end else
+        } // end while
+} // end listening
+
+int main() {
+        // clear chat history file
+        FILE *fp = fopen("chat_history", "w");
+        fclose(fp);
+
+        int fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0) report("socket", 1); // terminate
+
+        // bind server's local address in memory
+        struct sockaddr_in saddr;
+        memset(&saddr, 0, sizeof(saddr));       // clear the bytes
+        saddr.sin_family = AF_INET;
+        saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        saddr.sin_port = htons(PortNumber);
+
+        if (bind(fd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)
+                report("bind", 1); // terminate
+
+        if (listen(fd, MaxConnects) < 0) // listen for clients up to MaxConnects
+                report("listen", 1); // terminate
+
+        fprintf(stderr, "Listening on port %i for clients...\n", PortNumber);
+
+        // spawn new thread for listening
+        pthread_t listenThread;
+        int retVal = pthread_create(&listenThread, NULL, listening, (void *) &fd);
+
+        // close everything once listening is done
+        pthread_join(listenThread, NULL);
+        for (int i = 0; i < MaxConnects; ++i) {
+                if (clientArr[i] > 0) { // only valid addresses
+                        close(clientArr[i]);
+                        pthread_join(threads[i], NULL);
+                } // end if
+        } // end for
+        close(fd);
+
+        return 0;
+} // end main
